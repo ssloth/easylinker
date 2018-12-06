1ce72e0c9a4db5b1463cf22ed9c6eaa4a9bd48a3
SSnewbie <1035041075@qq.com>
2018-12-06 18:32:01 +0800

    fix:window下大小写不敏感导致的问题和jslint修复代码风格

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store/index";
-import Element from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import "./icons"; // icon
-import "@/styles/index.scss"; // global css
-import "./plugins/element-variables.scss";
-import "./permission"; // permission control
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './icons' // icon
+import '@/styles/index.scss' // global css
+import './plugins/element-variables.scss'
+import './permission' // permission control
 
-Vue.use(Element);
-Vue.config.productionTip = false;
+Vue.use(Element)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
