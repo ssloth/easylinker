725b1f8c85b23b1f34ca11b456e1ebce7628e116
zhaolei <zhaolei@thunk.cn>
2019-01-04 12:53:35 +0800

    更新服务端地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://118.178.59.56:2500/", //对应自己的接口
+        target: "http://45.40.194.63:2500/", //对应自己的接口
         // target: "http://192.168.1.106:2500/", //对应自己的接口
         changeOrigin: true,
         ws: true,
